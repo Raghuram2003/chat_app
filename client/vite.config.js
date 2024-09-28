ac2efae00b58da27ec5b584d31a5c60a6e35bd39
Raghuram2003 <destrobroxd@gmail.com>
2024-09-28 09:25:24 +0530

    dev

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": "chat-app-vcsg-2xp4v26oz-raghurams-projects.vercel.app/",
-    },
+  server : {
+    proxy : {
+      "/api" : "http://localhost:4040/",
+    }
   },
   plugins: [react()],
-});
+})
